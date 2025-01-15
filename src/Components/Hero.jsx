44322bac51428cf773f2757208b2b96fa46e7b45
TunmiDev <adewoleoluwatunmise@gmail.com>
2025-01-15 21:33:37 +0100

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import image from "../assets/cover.png";
+
 const Hero = () => {
   return (
-    <div>
-      {/*texts*/}
-      <h1 className=" pb-10 pt-20 pl-32 pr-96 font-medium text-7xl">
-        I explore the intersection of nature and technology
-      </h1>
-      {/*More About Me Links */}
-      <ul>
-        <li>
-          <a href="#">Get to know me</a>
-        </li>
-        <li>
-          <a href="#">Follow my Newsletter</a>{" "}
-        </li>
-      </ul>
-      {/*Image */}
-      <img src={image} alt="" />
+    <div className="flex items-start">
+      {/* Text Section */}
+      <div className="pr-10">
+        <h1 className="pb-10 pt-20 pr-32 font-medium text-7xl">
+          I explore the intersection of nature and technology
+        </h1>
+        {/* More About Me Links */}
+        <ul>
+          <li>
+            <a href="#">Get to know me</a>
+          </li>
+          <li>
+            <a href="#">Follow my Newsletter</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Image Section */}
+      <div className="flex-shrink-0">
+        <img src={image} alt="Cover" className="w-96 h-96 object-cover" />
+      </div>
     </div>
   );
 };
