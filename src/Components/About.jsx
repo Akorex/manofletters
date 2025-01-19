1db6752db71c9644bee7ca8d65d23ce46d0ecd17
TunmiDev <adewoleoluwatunmise@gmail.com>
2025-01-19 04:16:49 +0100

    ..

---
+++ src/Components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <p>
+        Hello! I'm Adewole Akorede, a passionate backend developer with [X
+        years] of experience in building efficient, scalable, and reliable
+        systems. I specialize in [list technologies/frameworks, e.g., Node.js,
+        Python, SQL, etc.] and enjoy solving complex problems to create seamless
+        digital experiences. When I'm not coding, you'll find me exploring
+        [hobbies, e.g., photography, gaming, etc.] or learning about the latest
+        trends in tech. I'm always excited to collaborate on projects that make
+        a difference. Let's build something amazing together!
+      </p>
+    </div>
+  );
+};
+
+export default About;
