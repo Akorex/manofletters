f2bd5c2a48ff7d939ae7ce69c1b637fe21e720d5
TunmiDev <adewoleoluwatunmise@gmail.com>
2025-01-21 22:56:00 +0100

    added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,18 +6,18 @@ const Hero = () => {
   return (
     <div className="flex items-center md:items-start p-6 md:p-16 flex-col-reverse md:flex-row ">
       {/* Text Section */}
-      <div className="pr-10">
+      <div className="text-center md:text-left md:pr-10">
         <h1 className="pb-10 pt-6 pl-20 font-medium text-6xl">
           I explore the world <br />
           through the lens of AI, Software and Technology.
         </h1>
         {/* More About Me Links */}
-        <ul className="pl-20 text-2xl text-gray-600 hover:cursor-pointer font-mono space-y-4 tracking-widest">
-          <li>
+        <ul className="pl-20 text-xl md:text-2xl text-gray-600 hover:cursor-pointer font-mono space-y-4 tracking-widest">
+          <li className="hover:text-gray-800">
             <Link to="/about">Get to know me</Link>
             <span className="ml-2">-&gt;</span>
           </li>
-          <li>
+          <li className="hover:text-gray-800">
             <Link to="/newsletter">Follow my newsletter</Link>
             <span className="ml-2">-&gt;</span>
           </li>
