4e2905c93adf0fbfec65d1f88d1416013e17a9f2
TunmiDev <adewoleoluwatunmise@gmail.com>
2025-01-21 23:02:19 +0100

    added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,11 +11,21 @@ const Navbar = () => {
         </div>
         {/*Navigation Links  */}
         <ul className="hidden flex-1 md:flex gap-4 text-gray-600 justify-end  font-semibold">
-          <Link to="/writing">writing</Link>
-          <Link to="/about">about</Link>
-          <Link to="/photos">photos</Link>
-          <Link to="/machine learning">machine learning</Link>
-          <Link to="/blog">blog</Link>
+          <Link to="/writing" className="hover:text-gray-950">
+            writing
+          </Link>
+          <Link to="/about" className="hover:text-gray-950">
+            about
+          </Link>
+          <Link to="/photos" className="hover:text-gray-950">
+            photos
+          </Link>
+          <Link to="/machine learning" className="hover:text-gray-950">
+            machine learning
+          </Link>
+          <Link to="/blog" className="hover:text-gray-950">
+            blog
+          </Link>
         </ul>
       </nav>
     </div>
