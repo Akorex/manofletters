1ad595e1eb0c0a3eda344bce23045071dd21e0a6
TunmiDev <adewoleoluwatunmise@gmail.com>
2025-01-21 22:45:16 +0100

    added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,14 +4,15 @@ import image from "../assets/cover.png";
 
 const Hero = () => {
   return (
-    <div className="flex items-start">
+    <div className="flex items-center md:items-start p-6 md:p-16 flex-col-reverse md:flex-row ">
       {/* Text Section */}
       <div className="pr-10">
-        <h1 className="pb-10 pt-20 pl-32 font-medium text-7xl">
-          I explore the world through the lens of AI, Software and Technology.
+        <h1 className="pb-10 pt-6 pl-20 font-medium text-6xl">
+          I explore the world <br />
+          through the lens of AI, Software and Technology.
         </h1>
         {/* More About Me Links */}
-        <ul className="pl-32 text-2xl text-gray-600 hover:cursor-pointer font-mono space-y-4 tracking-widest">
+        <ul className="pl-20 text-2xl text-gray-600 hover:cursor-pointer font-mono space-y-4 tracking-widest">
           <li>
             <Link to="/about">Get to know me</Link>
             <span className="ml-2">-&gt;</span>
@@ -28,7 +29,7 @@ const Hero = () => {
         <img
           src={image}
           alt="Cover"
-          className="w-96 h-120 pt-28 pr-12 rounded-lg object-cover "
+          className="w-96 h-120 pt-6 pr-12 rounded-lg object-cover "
         />
       </div>
     </div>
