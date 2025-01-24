b46827809086d97e3070dfe8a472779733c36ff0
TunmiDev <adewoleoluwatunmise@gmail.com>
2025-01-24 22:21:50 +0100

    added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,11 +4,11 @@ const About = () => {
   return (
     <div className="bg-[#fdf9f3] min-h-screen flex flex-col  px-6 py-12 md:px-24">
       <div className="max-w-4xl">
-        <h1 className="text-4xl md:text-6xl font-bold text-gray-900 mb-6 ">
+        <h1 className="text-4xl md:text-4xl font-bold text-gray-900 pl-8 pt-12">
           About
         </h1>
       </div>
-      <div className="pb-10 pt-6 pl-6 font-medium text-4xl">
+      <div className="pb-10 pt-3 pl-8 text-4xl">
         <p>Hello, I'm Akorede Adewole.</p>
         <p>Software Engineer</p>
       </div>
