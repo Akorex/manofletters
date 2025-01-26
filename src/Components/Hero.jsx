4a9cccb4b56a48160207e932d6ccc9de0381dd23
TunmiDev <adewoleoluwatunmise@gmail.com>
2025-01-26 08:55:51 +0100

    added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,13 +7,18 @@ const Hero = () => {
     <div className="flex items-center md:items-start p-6 md:p-16 flex-col-reverse md:flex-row ">
       {/* Text Section */}
       <div className="text-center md:text-left md:pr-10">
-        <h1 className="pb-10 pt-6 pl-20 font-medium text-4xl">
+        <h1 className="pb-10 pt-6 pl-20 font-medium text-5xl">
           Hello, I'm Akorede Adewole
           <br />
           Software Engineer{" "}
           <span className="font-extrabold text-2xl">&mdash;&mdash;</span>Backend
           & Machine Learning.
         </h1>
+        <p>
+          <p className="pb-10 pl-20 font-normal text-3xl">
+            I explore the world through the lens of AI, Software and Technology
+          </p>
+        </p>
         {/* More About Me Links */}
         <ul className="pl-20 text-xl md:text-2xl text-gray-600 hover:cursor-pointer font-mono space-y-4 tracking-widest">
           <li className="hover:text-gray-800">
