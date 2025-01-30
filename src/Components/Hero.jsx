7b7c5ec827b0c135ec377ec7b824d50f1e5231e4
TunmiDev <adewoleoluwatunmise@gmail.com>
2025-01-30 12:05:22 +0100

    added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,23 +4,25 @@ import image from "../assets/cover.png";
 
 const Hero = () => {
   return (
-    <div className="flex items-center md:items-start p-6 md:p-16 flex-col-reverse md:flex-row ">
+    <div className="flex flex-col md:flex-row items-center justify-between px-6 md:px-16 py-10">
       {/* Text Section */}
-      <div className="text-center md:text-left md:pr-10">
-        <h1 className="pb-10 pt-6 pl-20 font-medium text-5xl">
+      <div className="text-center md:text-left md:max-w-2xl">
+        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-medium leading-tight">
           Hello, I'm Akorede Adewole
           <br />
-          Software Engineer{" "}
-          <span className="font-extrabold text-2xl">&nbsp;—&nbsp;</span>Backend
-          & Machine Learning.
+          <span className="block">Software Engineer</span>
+          <span className="font-extrabold text-xl sm:text-2xl">
+            &nbsp;—&nbsp;
+          </span>
+          Backend & Machine Learning.
         </h1>
-        <p>
-          <p className="pb-10 pl-20 font-normal text-3xl">
-            I explore the world through the lens of AI, Software and Technology
-          </p>
+
+        <p className="mt-6 text-lg sm:text-2xl text-gray-700">
+          I explore the world through the lens of AI, Software, and Technology.
         </p>
+
         {/* More About Me Links */}
-        <ul className="pl-20 text-xl md:text-2xl text-gray-600 hover:cursor-pointer font-mono space-y-4 tracking-widest">
+        <ul className="mt-6 text-lg sm:text-xl text-gray-600 hover:cursor-pointer font-mono space-y-4 tracking-widest">
           <li className="hover:text-gray-800">
             <Link to="/about">Get to know me</Link>
             <span className="ml-2">-&gt;</span>
@@ -33,11 +35,11 @@ const Hero = () => {
       </div>
 
       {/* Image Section */}
-      <div className="flex-shrink-0">
+      <div className="mt-10 md:mt-0 flex-shrink-0">
         <img
           src={image}
           alt="Cover"
-          className="w-96 h-120 pt-6 pr-12 rounded-lg object-cover "
+          className="w-64 sm:w-80 md:w-96 lg:w-[450px] rounded-lg object-cover"
         />
       </div>
     </div>
